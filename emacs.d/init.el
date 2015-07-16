4628277b65f61a9f17d88dc592a075e37400b161
Cristián Romo <cristian.g.romo@gmail.com>
2015-07-16 10:24:08 -0500

    Update to current emacs initialization.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,123 @@
+(setq inhibit-default-init t)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
+;; enable visual feedback on selections
+(setq transient-mark-mode t)
+
+;; default to better frame titles
+(setq frame-title-format
+      (concat  "%b - emacs@" (system-name)))
+
+;; default to unified diffs
+(setq diff-switches "-u")
+
+;; PuTTY hack - needs to be in SCO mode
+(if (eq system-uses-terminfo t)
+    (progn
+      (define-key key-translation-map [\e] [\M])
+      (define-key input-decode-map "\e[H" [home])
+      (define-key input-decode-map "\e[F" [end])
+      (define-key input-decode-map "\e[D" [S-left])
+      (define-key input-decode-map "\e[C" [S-right])
+      (define-key input-decode-map "\e[A" [S-up])
+      (define-key input-decode-map "\e[B" [S-down])
+      (define-key input-decode-map "\e[C" [S-right])
+      (define-key input-decode-map "\e[I" [prior])
+      (define-key input-decode-map "\e[G" [next])
+      (define-key input-decode-map "\e[M" [f1])
+      (define-key input-decode-map "\e[Y" [S-f1])
+      (define-key input-decode-map "\e[k" [C-f1])
+      (define-key input-decode-map "\e\e[M" [M-f1])
+      (define-key input-decode-map "\e[N" [f2])
+      (define-key input-decode-map "\e[Z" [S-f2])
+      (define-key input-decode-map "\e[l" [C-f2])
+      (define-key input-decode-map "\e\e[N" [M-f2])
+      (define-key input-decode-map "\e[O" [f3])
+      (define-key input-decode-map "\e[a" [S-f3])
+      (define-key input-decode-map "\e[m" [C-f3])
+      (define-key input-decode-map "\e\e[O" [M-f3])
+      (define-key input-decode-map "\e[P" [f4])
+      (define-key input-decode-map "\e[b" [S-f4])
+      (define-key input-decode-map "\e[n" [C-f4])
+      (define-key input-decode-map "\e\e[P" [M-f4])
+      (define-key input-decode-map "\e[Q" [f5])
+      (define-key input-decode-map "\e[c" [S-f5])
+      (define-key input-decode-map "\e[o" [C-f5])
+      (define-key input-decode-map "\e\e[Q" [M-f5])
+      (define-key input-decode-map "\e[R" [f6])
+      (define-key input-decode-map "\e[d" [S-f6])
+      (define-key input-decode-map "\e[p" [C-f6])
+      (define-key input-decode-map "\e\e[R" [M-f6])
+      (define-key input-decode-map "\e[S" [f7])
+      (define-key input-decode-map "\e[e" [S-f7])
+      (define-key input-decode-map "\e[q" [C-f7])
+      (define-key input-decode-map "\e\e[S" [M-f7])
+      (define-key input-decode-map "\e[T" [f8])
+      (define-key input-decode-map "\e[f" [S-f8])
+      (define-key input-decode-map "\e[r" [C-f8])
+      (define-key input-decode-map "\e\e[T" [M-f8])
+      (define-key input-decode-map "\e[U" [f9])
+      (define-key input-decode-map "\e[g" [S-f9])
+      (define-key input-decode-map "\e[s" [C-f9])
+      (define-key input-decode-map "\e\e[U" [M-f9])
+      (define-key input-decode-map "\e[V" [f10])
+      (define-key input-decode-map "\e[h" [S-f10])
+      (define-key input-decode-map "\e[_" [C-f10])
+      (define-key input-decode-map "\e\e[V" [M-f10])
+      (define-key input-decode-map "\e[W" [f11])
+      (define-key input-decode-map "\e[i" [S-f11])
+      (define-key input-decode-map "\e[u" [C-f11])
+      (define-key input-decode-map "\e\e[W" [M-f11])
+      (define-key input-decode-map "\e[X" [f12])
+      (define-key input-decode-map "\e[j" [S-f12])
+      (define-key input-decode-map "\e[v" [C-f12])
+      (define-key input-decode-map "\e\e[X" [M-f12])))
+
+;; Set to non-GNU C++ indentation.
+(defun my-c++-mode-hook ()
+  (c-set-offset 'substatement-open 0)
+  (setq c++-tab-always-indent t)
+  (setq c-basic-offset 4)
+  (setq c-indent-level 4)
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil))
+(add-hook 'c-mode-hook 'my-c++-mode-hook)
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; Disable the menu bar, since I can't click them in a terminal anyway.
+(menu-bar-mode -1)
+
+;; Turn on the ruler
+(ruler-mode)
+
+;; Turn on the column display in the mode line.
+(column-number-mode)
+
+
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+(put 'dired-find-alternate-file 'disabled nil)
 
 (require 'tramp)
-
 (when (string-equal system-type "windows-nt")
   (setq tramp-default-method "plink"))
+
+; Load package manager
+(when (load (expand-file-name "~/.emacs.d/package.el")))
+(require 'package)
+(add-to-list 'package-archives '("elpa" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(require 'evil)
+(evil-mode 1)
+
+(when (load (expand-file-name "~/.emacs.d/key-chord.el")))
+(require 'key-chord)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+(key-chord-mode 1)
